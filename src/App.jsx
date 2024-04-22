358a7a6762a03c051affe22ab173a2143334a5bd
Coder Talha <talhacust7@gmail.com>
2024-04-22 19:36:53 +0500

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { Suspense, useContext, useRef } from "react";
+import { useContext, useRef } from "react";
 import Home from "./components/Home";
 import Webgl from "./components/Webgl";
 import { DataContext } from "./context/DataContext";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import nft from "../src/assets/ntf.jpg";
-import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 function App() {
   const { activeData } = useContext(DataContext);
@@ -20,20 +18,18 @@ function App() {
   }, [activeData]);
 
   return (
-    <Suspense fallback={<Loader />}>
-      <div ref={ref} className="w-screen h-screen relative ">
-        <h1
-          className="select-none absolute z-50 text-3xl font-bold text-left tracking-tight my-2 ml-6 md:ml-28 lg:ml-[5vw] lg:my-8"
-          style={{ color: activeData.logo }}
-        >
-          NFT's.
-        </h1>
-        <div className=" w-full h-screen flex justify-between items-center flex-col lg:flex-row-reverse">
-          <Webgl />
-          <Home />
-        </div>
+    <div ref={ref} className="w-screen h-screen relative ">
+      <h1
+        className="select-none absolute z-50 text-3xl font-bold text-left tracking-tight my-2 ml-6 md:ml-28 lg:ml-[5vw] lg:my-8"
+        style={{ color: activeData.logo }}
+      >
+        NFT's.
+      </h1>
+      <div className=" w-full h-screen flex justify-between items-center flex-col lg:flex-row-reverse">
+        <Webgl />
+        <Home />
       </div>
-    </Suspense>
+    </div>
   );
 }
 
--- src/components/Webgl.jsx
+++ src/components/Webgl.jsx
@@ -1,9 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./Experience";
 import Swap from "./Swap";
+import { Loader } from "@react-three/drei";
 import { Suspense } from "react";
 
 function Webgl() {
+
   return (
     <div className="w-full h-3/5 relative z-10 lg:w-1/2 lg:h-full ">
       <Canvas shadows camera={{ position: [0, 0, 4], fov: 45 }}>
@@ -11,6 +13,7 @@ function Webgl() {
           <Experience />
         </Suspense>
       </Canvas>
+      <Loader />
       <Swap />
     </div>
   );
